9d9a442720b02c0e3baf4dca5205f7e32330d299
root <root@ns3285734.ip-5-135-181.eu>
2019-01-03 14:39:12 +0100

    _

---
+++ install/mssql-server.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+script_name=`basename "$0"`
+
+if [[ "$USER" != 'root' ]]; then
+    echo "./$script_name must be run as root user"
+    exit 1
+fi
+
+if [ "$#" -ne 3 ]; then
+    echo "./$script_name [MSSQL_SA_PASSWORD] [NETPIPS_LOGIN_PASSWORD] [NETPIPS_SUPERADMIN_EMAIL]"
+    exit 1
+fi
+MSSQL_SA_PASSWORD=$1
+NETPIPS_LOGIN_PASSWORD=$2
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2017.list)"
+sudo apt-get update
+sudo apt-get install -y mssql-server
+service mssql-server stop
+sudo ACCEPT_EULA='Y' MSSQL_PID='Express' MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD /opt/mssql/bin/mssql-conf -n setup
+systemctl status mssql-server || true
+
+# sqlcmd
+echo "installing sqlcmd"
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+sudo apt-get update
+sudo ACCEPT_EULA='Y' DEBIAN_FRONTEND=noninteractive apt-get install -y mssql-tools unixodbc-dev
+PATH="$PATH:/opt/mssql-tools/bin"
+
+sqlcmd -U sa -P $MSSQL_SA_PASSWORD -Q "CREATE DATABASE netpips"
+sqlcmd -U sa -P $MSSQL_SA_PASSWORD -Q "CREATE LOGIN netpips WITH PASSWORD = '$NETPIPS_LOGIN_PASSWORD', CHECK_EXPIRATION = OFF, CHECK_POLICY = OFF;"
+# db user
+# insert SuperAdmin
