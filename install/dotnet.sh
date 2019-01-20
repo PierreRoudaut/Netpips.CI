c1a7b9b55ac193a5e887646f856c3a3a8c446ac1
Pierre Roudaut <pierreroudaut@gmail.com>
2019-01-20 18:03:40 +0100

    _

--- install/dotnet.sh
+++ install/dotnet.sh
@@ -2,13 +2,6 @@
 
 set -e
 
-script_name=`basename "$0"`
-
-if [[ "$USER" != 'root' ]]; then
-    echo "./$script_name must be run as root user"
-    exit 1
-fi
-
 wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm -f packages-microsoft-prod.deb
