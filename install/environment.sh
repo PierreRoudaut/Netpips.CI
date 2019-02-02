dbd3a14c0656d1bcab9c8582062da93f18abe7a1
Pierre Roudaut <pierreroudaut@gmail.com>
2019-02-02 20:10:27 +0100

    3

--- install/environment.sh
+++ install/environment.sh
@@ -5,16 +5,11 @@ set -ex
 : "${ENVIRONMENT_ID:?ENVIRONMENT_ID not set}"
 
 env_url="https://drive.google.com/uc?export=download&id=$ENVIRONMENT_ID"
-
 env=${ASPNETCORE_ENVIRONMENT,}
-echo $env
-echo /shared/Netpips.PWA/src/environments/environment.$env.settings.json
-
-rm -f /shared/Netpips.PWA/src/environments/environment.$env.settings.json
-ls -la  /shared/Netpips.PWA/src/environments/
+rm -f /shared/Netpips.PWA/src/environments/environment.$env.ts
 
 if [[ "$USER" = 'root' ]]; then
-    su - netpips -c "cd /shared/Netpips.PWA/src/environments && curl -L '$appsettings_url' -O -J 2> /dev/null"
+    su - netpips -c "cd /shared/Netpips.PWA/src/environments && curl -L '$env_url' -O -J"
 else
     cd /shared/Netpips.PWA/src/environments && curl -L "$env_url" -O -J
 fi
