de50a74f5e61712489fd09b0d3dd2ad347a606cc
Pierre Roudaut <PierreRoudaut@users.noreply.github.com>
2021-10-06 12:37:44 +0200

    Update mssql-server.sh

--- install/mssql-server.sh
+++ install/mssql-server.sh
@@ -23,7 +23,7 @@ sudo systemctl status mssql-server || true
 # sqlcmd
 echo "installing sqlcmd"
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 sudo apt-get update
 sudo ACCEPT_EULA='Y' DEBIAN_FRONTEND=noninteractive apt-get install -y mssql-tools unixodbc-dev
 PATH="$PATH:/opt/mssql-tools/bin"
