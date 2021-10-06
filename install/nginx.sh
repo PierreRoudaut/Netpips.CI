95b975b3558287be3e829e03b1acc6de5df78cb5
Pierre Roudaut <PierreRoudaut@users.noreply.github.com>
2021-10-06 17:39:10 +0200

    Update nginx.sh

--- install/nginx.sh
+++ install/nginx.sh
@@ -1,45 +1,34 @@
 #!/bin/bash
 #
 # - Installs nginx
-# - Sets up http routes for client and server
-# - Installs certbot
-# - Sets up https on domain
 #
 #############################
 
 set -eu
 
-conf='netpips.conf'
-: "${DOMAIN:?DOMAIN not set}"
-: "${CERTBOT_CONTACT_EMAIL:?CERTBOT_CONTACT_EMAIL not set}"
-
-apt-get install -y nginx
-service nginx stop || true
-rm -f /etc/nginx/sites-available/*
-rm -f /etc/nginx/sites-enabled/*
-
-cat $conf.tmpl | sed s/{{domain}}/$DOMAIN/g > /etc/nginx/sites-available/$conf
-ln -s /etc/nginx/sites-available/$conf /etc/nginx/sites-enabled/$conf
-
-sudo service nginx start
-service nginx status
-
-
-set +e
-certbot --help
-res=$?
-set -e
-if [ "$res" -ne 0 ]; then
-    sudo apt-get update -y
-    sudo apt-get install software-properties-common -y
-    sudo add-apt-repository universe -y
-    sudo add-apt-repository ppa:certbot/certbot -y
-    sudo apt-get update -y
-    sudo apt-get install python-certbot-nginx -y
+script_name=`basename "$0"`
+if [ "$#" -ne 2 ]; then
+    echo "./$script_name [DOMAIN] [NGINX_CONF_TMPL_PATH]"
+    exit 1
 fi
 
-certbot --nginx --domains $DOMAIN --non-interactive --redirect --agree-tos -m $CERTBOT_CONTACT_EMAIL
-nginx -s reload || true
-systemctl daemon-reload
-service nginx status
+DOMAIN=$1
+NGINX_CONF_TMPL_PATH=$2
 
+if [[ ! -f '/var/netpips/client/index.html' ]]; then
+    sudo mkdir -p /var/netpips/client
+    sudo echo 'hello world' > /var/netpips/client/index.html
+    chown -R netpips:netpips /var/netpips/client
+fi
+
+
+sudo apt-get install -y nginx
+sudo service nginx stop || true
+sudo rm -f /etc/nginx/sites-available/*
+sudo rm -f /etc/nginx/sites-enabled/*
+
+cat $NGINX_CONF_TMPL_PATH | sudo sed s/{{domain}}/$DOMAIN/g > /etc/nginx/sites-available/netpips.conf
+sudo ln -s /etc/nginx/sites-available/netpips.conf /etc/nginx/sites-enabled/netpips.conf
+
+sudo service nginx start
+sudo service nginx status
