5118b86a80dc12a88a2a1e7ba00dee904a9c3b88
Pierre Roudaut <PierreRoudaut@users.noreply.github.com>
2021-10-06 16:40:30 +0200

    Update transmission.sh

--- install/transmission.sh
+++ install/transmission.sh
@@ -7,17 +7,17 @@
 
 set -e
 
-apt-get install -y transmission-cli
-apt-get install -y transmission-daemon
-apt-get install -y transmission-common
+sudo apt-get install -y transmission-cli
+sudo apt-get install -y transmission-daemon
+sudo apt-get install -y transmission-common
 
 user='netpips'
-service 'transmission-daemon' stop
-sed -i s/debian-transmission/$user/g /etc/init.d/transmission-daemon
-sed -i s/debian-transmission/$user/g /etc/init/transmission-daemon.conf
-sed -i s/debian-transmission/$user/g /lib/systemd/system/transmission-daemon.service
-chown -R "$user:$user" /var/lib/transmission-daemon
-chown -R "$user:$user" /etc/transmission-daemon
-systemctl daemon-reload ; sudo service 'transmission-daemon' start ; sleep 1
+sudo service 'transmission-daemon' stop
+sudo sed -i s/debian-transmission/$user/g /etc/init.d/transmission-daemon
+sudo sed -i s/debian-transmission/$user/g /etc/init/transmission-daemon.conf
+sudo sed -i s/debian-transmission/$user/g /lib/systemd/system/transmission-daemon.service
+sudo chown -R "$user:$user" /var/lib/transmission-daemon
+sudo chown -R "$user:$user" /etc/transmission-daemon
+sudo systemctl daemon-reload ; sudo service 'transmission-daemon' start ; sleep 1
 
 echo 'Transsmission => OK'
