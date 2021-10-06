71978b484c00fe010083a26b21bff1f3ccb8c14a
Pierre Roudaut <PierreRoudaut@users.noreply.github.com>
2021-10-06 17:52:06 +0200

    Update packages.sh

--- install/packages.sh
+++ install/packages.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-apt-get update -y
 
-for package in `cat packages.txt`
-do
-    apt-get install -y "$package" > /dev/null
-done
+sudo apt-get update -y
+sudo apt-get install -y bash-completion
+sudo apt-get install -y aria2
+sudo apt-get install -y emacs
+sudo apt-get install -y git
+sudo apt-get install -y htop
+sudo apt-get install -y jq
+sudo apt-get install -y mediainfo
+sudo apt-get install -y tree
+sudo apt-get install -y nodejs
+sudo apt-get install -y python3.9
+sudo apt-get install -y python3-pip
+sudo apt-get install -y python-is-python3
+sudo pip install -U cfscrape
+
+
 git config --global core.editor 'emacs'
