d302e6f346608e817a7d3689707137b91a29609f
Pierre Roudaut <PierreRoudaut@users.noreply.github.com>
2021-10-08 10:51:36 +0200

    Update netpips-server.sh

--- install/netpips-server.sh
+++ install/netpips-server.sh
@@ -7,15 +7,14 @@
 set -exu
 
 user='netpips'
-srvc='netpips-server'
 
-: "${ASPNETCORE_ENVIRONMENT:?ASPNETCORE_ENVIRONMENT not set}"
 
-systemctl disable $srvc.service || true
-rm -f /etc/systemd/system/$srvc.service
-cat $srvc.service.tmpl | sed s/{{env}}/$ASPNETCORE_ENVIRONMENT/g | sed s/{{user}}/$user/g  > /etc/systemd/system/$srvc.service
-systemctl daemon-reload
-systemctl enable $srvc.service
-service $srvc stop
+sudo systemctl disable netpips-server.service || true
+sudo rm -f /etc/systemd/system/netpips-server.service
+# sudo cat netpips-server.service.tmpl | sed s/{{user}}/$user/g  > /etc/systemd/system/netpips-server.service
+sudo cp netpips-server.service /etc/systemd/system/netpips-server.service
+sudo systemctl daemon-reload
+sudo systemctl enable netpips-server.service
+sudo service netpips-server start
 
 
